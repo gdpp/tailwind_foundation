8704843e0a80323f7ca7d8423d73ebf8e6810e47
gustavo <gustavo.perez.231191@gmail.com>
2023-11-05 21:51:25 -0600

    add secntions and components

---
+++ project/src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomersReviews from './CustomersReviews';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts'
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer'
+
+export {
+    CustomersReviews,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
